c20c77d8881d3d0fb55695b71a2ad4f016116dfb
seancummins99 <73312970+seancummins99@users.noreply.github.com>
2021-11-15 15:03:17 +0000

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+import numpy as np
+def fib(n):
+    Phi =((np.sqrt(5)+1)/2)
+    phi = Phi-1
+    fib_n = int(((Phi**n) - (-phi)**n)/np.sqrt(5))
+    return fib_n
+
+n = int(input('n = '))
+suffixes = ['st','nd','rd','th']
+
+if str(n)[-1] == '1':
+    suffix = suffixes[0]
+elif str(n)[-1] == '2':
+    suffix = suffixes[1]
+elif str(n)[-1] == '3':
+    suffix = suffixes[2]
+else:
+    suffix = suffixes[3]
+
+print(f'The {n}{suffix} Fibonacci number is {fib(n)}')
+    
